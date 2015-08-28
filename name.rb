e652a924e3968396020883131f03bb9dba7ac466
Roman Menzel <roman.menzel1@gmx.net>
2015-08-28 17:26:19 +0200

    Delete name.rb

--- name.rb
+++
@@ -1,5 +0,0 @@
-print "Type in your name: "
-name = gets.chomp
-greet = ["Hello", "What's up", "Hi", "Hey", "Nice to see you", "How are you", "Let's go"]
-
-puts "#{greet[rand(greet.size)]} #{name}"
